5392cbd192d4b4cc91713d4581dd43a9afbac221
ssu <gra.cce.ful@gmail.com>
2023-08-28 20:15:26 +0900

    수민 커밋

--- factorypjf/src/App.js
+++ factorypjf/src/App.js
@@ -4,12 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import Layout from "component/layout/Layout";
 import ProductionRoutes from "router/ProductionRoutes";
-import InboundRoutes from 'router/InboundRoutes'
-import StorageRoutes from 'router/StorageRoutes'
+import InboundRoutes from "router/InboundRoutes";
+import StorageRoutes from "router/StorageRoutes";
 import "./style/App.css";
 import "./style/Table.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Layout from "./component/layout/Layout";
 import Storage from "./component/management/storage/Storage";
 import Management from "./pages/Management";
 import Partner from "./component/management/partner/Partner";
@@ -17,12 +16,11 @@ import Item from "./component/management/item/Item";
 import Product from "./component/management/product/Product";
 import UnitPrice from "./component/management/unitPrice/UnitPrice";
 
-
 function App() {
   return (
     <Layout>
       <Routes>
-        <Route path="/production/*" element={<ProductionRoutes/>}/>
+        <Route path="/production/*" element={<ProductionRoutes />} />
         {/* <Route path="/inbound/*" element={<InboundRoutes/>}/>
         <Route path="/storage/*" element={<StorageRoutes/>}/> */}
         <Route path="/management" element={<Management />}>
--- factorypjf/src/router/StorageRoutes.js
+++ factorypjf/src/router/StorageRoutes.js
@@ -1,8 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function StorageRoutes() {
-  return (
-    <div>StorageRoutes</div>
-    <div>입고</div>
-  )
+  return <div>StorageRoutes</div>;
 }
